261e7a4ab3536c54bebdc08063c1f0c6639ef760
Nebiyou Hailemariam <nebHailemariam@gmail.com>
2020-08-15 20:41:38 +0300

    Building our React app

--- src/config.js
+++ src/config.js
@@ -13,4 +13,7 @@ export default {
     APP_CLIENT_ID: "7f1t6og0boekjie5mp2ihri6nv",
     IDENTITY_POOL_ID: "us-east-2:bb2728ea-b5a9-4ac1-881c-ff2ae545743e",
   },
+  MAX_ATTACHMENT_SIZE: 5000000,
+  STRIPE_KEY:
+    "pk_test_51HEP9wDtye4ifCWOVargMootU8aVSkWjQdCIGMed2Xeku3xX4x3TdjjCr7fBfBIRrazKEAdesLCuXoRJTRIRVC1x00RSoaAT6a",
 };
